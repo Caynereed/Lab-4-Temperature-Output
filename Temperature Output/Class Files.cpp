b78b1974c9cadfc1021724b32e8e562bcba4082e
Caynereed <reed146@tritons.iowacentral.edu>
2023-10-31 22:23:07 -0500

    Create Class Files.cpp

---
+++ Temperature Output/Class Files.cpp
@@ -0,0 +1,19 @@
+#include "Class Files.hpp"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+class TemperatureChart;
+
+void TemperatureChart::setstarCount(double getstarCount_)
+{
+   // setstarCount = (int)(getTemperature()/3);
+}
+
+double TemperatureChart::getstarCount()
+{
+   return 0;
+}
+
